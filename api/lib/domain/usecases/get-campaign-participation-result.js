b405a3059aaea57c937caf6e5d3e2b6d3a8018e0
Laura Bergoens <laura.bergoens@pix.fr>
2021-02-04 17:10:58 +0100

    refactor: remove unneccessary check since this route is used solely by the owner of the participation

--- api/lib/domain/usecases/get-campaign-participation-result.js
+++ api/lib/domain/usecases/get-campaign-participation-result.js
@@ -8,12 +8,12 @@ module.exports = async function getCampaignParticipationResult({
   badgeAcquisitionRepository,
   campaignParticipationRepository,
   campaignParticipationResultRepository,
-  campaignRepository,
   targetProfileRepository,
 }) {
-
   const campaignParticipation = await campaignParticipationRepository.get(campaignParticipationId);
-  await _checkIfUserHasAccessToThisCampaignParticipation(userId, campaignParticipation, campaignRepository);
+  if (userId !== campaignParticipation.userId) {
+    throw new UserNotAuthorizedToAccessEntity('User does not have access to this campaign participation');
+  }
 
   const targetProfile = await targetProfileRepository.getByCampaignId(campaignParticipation.campaignId);
   const campaignBadges = await badgeRepository.findByTargetProfileId(targetProfile.id);
@@ -23,15 +23,3 @@ module.exports = async function getCampaignParticipationResult({
 
   return campaignParticipationResultRepository.getByParticipationId(campaignParticipationId, campaignBadges, acquiredBadgeIds, locale);
 };
-
-async function _checkIfUserHasAccessToThisCampaignParticipation(userId, campaignParticipation, campaignRepository) {
-  const campaignParticipationBelongsToUser = (userId === campaignParticipation.userId);
-  const userIsMemberOfCampaignOrganization = await campaignRepository.checkIfUserOrganizationHasAccessToCampaign(
-    campaignParticipation.campaignId,
-    userId,
-  );
-
-  if (!campaignParticipationBelongsToUser && !userIsMemberOfCampaignOrganization) {
-    throw new UserNotAuthorizedToAccessEntity('User does not have access to this campaign participation');
-  }
-}
